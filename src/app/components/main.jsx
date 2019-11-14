9f395052a7234763e227b6cd2166f9ce0d7793df
sathyalog <sathya.vakacharla@gmail.com>
2019-11-14 17:04:40 +0530

    added basic navigation

--- src/app/components/main.jsx
+++ src/app/components/main.jsx
@@ -4,14 +4,18 @@ import {store} from '../store';
 import { ConnectedDashboard } from './dashboard';
 import {Router, Route} from 'react-router-dom';
 import {history} from './../store/history';
+import {ConnectedNavigation} from './Navigation';
 
 export const Main = () => (
     <Router history={history}>
         <Provider store={store}>
+            <div>
+            <ConnectedNavigation />
             <Route
                 exact path="/dashboard"
                 render={() => (<ConnectedDashboard />)}
             />
+            </div>
         </Provider>
     </Router>
 )
