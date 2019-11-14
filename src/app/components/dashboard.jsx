cd9114dd71044ffeff626b4b3505ef7328b5215f
sathyalog <sathya.vakacharla@gmail.com>
2019-11-14 16:04:45 +0530

    tasks list with group names added

--- src/app/components/dashboard.jsx
+++ src/app/components/dashboard.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {ConnectedTaskList} from './TaskList';
 
 export const Dashboard = ({groups}) => (
     <div>
         <h2>Dashboard</h2>
         {groups.map(group => (
-            <div>{group.name}</div>
-        )
+                <ConnectedTaskList id={group.id} name={group.name}/>
+            )
         )}
     </div>
 )
