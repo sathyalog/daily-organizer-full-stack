95df10131c203b2781bfff635045a2a922044760
sathyalog <sathya.vakacharla@gmail.com>
2019-11-14 16:30:52 +0530

    router enabled

--- src/app/components/main.jsx
+++ src/app/components/main.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import {store} from '../store';
 import { ConnectedDashboard } from './dashboard';
+import {Router, Route} from 'react-router-dom';
+import {history} from './../store/history';
 
 export const Main = () => (
-    <Provider store={store}>
-        <ConnectedDashboard />
-    </Provider>
+    <Router history={history}>
+        <Provider store={store}>
+            <Route
+                exact path="/dashboard"
+                render={() => (<ConnectedDashboard />)}
+            />
+        </Provider>
+    </Router>
 )
