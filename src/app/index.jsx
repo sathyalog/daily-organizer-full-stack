bdb81cf645f180f76210f83ece82bd141a90f0b0
sathyalog <sathya.vakacharla@gmail.com>
2019-11-14 14:40:14 +0530

    configured store for dashboard

---
+++ src/app/index.jsx
@@ -0,0 +1,11 @@
+
+import {store} from './store';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Main} from './components/main';
+
+ReactDOM.render(
+    <Main />,
+    document.getElementById('app')
+)
